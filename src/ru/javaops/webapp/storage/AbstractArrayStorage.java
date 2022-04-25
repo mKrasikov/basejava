faf8d78debdf004644637ca377c00d690371b415
Mikhail Krasikov <mishkras@mail.ru>
2022-04-25 13:26:04 +0300

    HW3:first try

---
+++ src/ru/javaops/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,79 @@
+package ru.javaops.webapp.storage;
+
+import ru.javaops.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10000;
+    protected final Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    protected abstract void fillDeletedElement(int index);
+
+    protected abstract void insertElement(Resume r, int index);
+
+    protected abstract int getIndex(String uuid);
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+        System.out.println("There are no resumes now.");
+    }
+
+    public void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            System.out.println("The resume \"" + r + "\" is already exists.");
+        } else if (size == STORAGE_LIMIT) {
+            System.out.println("The storage is full.");
+        } else {
+            insertElement(r, index);
+            size++;
+        }
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            System.out.println("There is no \"" + r + "\" resume.");
+        } else {
+            storage[index] = r;
+            System.out.println("The resume \"" + r + "\" is update.");
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("There is no \"" + uuid + "\" such resume.");
+            return null;
+
+        }
+        System.out.println("Resume \"" + uuid + "\" is found.");
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("There is no \"" + uuid + "\" such resume.");
+
+        } else {
+            fillDeletedElement(index);
+            System.out.println("Resume \"" + uuid + "\" is removed.");
+            size--;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+}
--- src/ru/javaops/webapp/storage/ArrayStorage.java
+++ src/ru/javaops/webapp/storage/ArrayStorage.java
@@ -2,82 +2,27 @@ package ru.javaops.webapp.storage;
 
 import ru.javaops.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage {
 
-    private int findIndex(String uuid) {
+    @Override
+    protected void fillDeletedElement(int index) {
+        storage[index] = storage[size - 1];
+    }
+
+    @Override
+    protected void insertElement(Resume r, int index) {
+        storage[size] = r;
+    }
+
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
         return -1;
-    }
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-        System.out.println("There are no resumes now.");
-    }
-
-    public void save(Resume r) {
-        int index = findIndex(r.getUuid());
-        if (size == storage.length) {
-            System.out.println("The storage is full.");
-        } else if (index == -1) {
-            storage[size] = r;
-            System.out.println("The resume \"" + r + "\" is save.");
-            size++;
-        } else {
-            System.out.println("The resume \"" + r + "\" is already exists.");
-        }
-    }
-
-    public void update(Resume r) {
-        int index = findIndex(r.getUuid());
-        if (index != -1) {
-            storage[index] = r;
-            System.out.println("The resume \"" + r + "\" is update.");
-        } else {
-            System.out.println("There is no \"" + r + "\" resume.");
-        }
-    }
-
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index != -1) {
-            System.out.println("Resume \"" + uuid + "\" is found.");
-            return storage[index];
-        }
-        System.out.println("There is no \"" + uuid + "\" such resume.");
-        return null;
-    }
-
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index != -1) {
-            System.arraycopy(storage, index + 1, storage, index, storage.length - (index + 1));
-            System.out.println("Resume \"" + uuid + "\" is removed.");
-            size--;
-        } else {
-            System.out.println("There is no \"" + uuid + "\" such resume.");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
     }
 }
