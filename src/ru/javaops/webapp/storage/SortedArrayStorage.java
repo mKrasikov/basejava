2cc90b291abd63d670b59633f20d8ad52f1fcd5f
Mikhail Krasikov <mishkras@mail.ru>
2022-04-25 18:36:18 +0300

    HW3: second try

--- src/ru/javaops/webapp/storage/SortedArrayStorage.java
+++ src/ru/javaops/webapp/storage/SortedArrayStorage.java
@@ -7,31 +7,6 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
-    public void clear() {
-
-    }
-
-    @Override
-    public void save(Resume r) {
-
-    }
-
-    @Override
-    public void update(Resume r) {
-
-    }
-
-    @Override
-    public void delete(String uuid) {
-
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return new Resume[0];
-    }
-
-    @Override
     protected void fillDeletedElement(int index) {
         int numMoved = size - index - 1;
         if (numMoved > 0) {
