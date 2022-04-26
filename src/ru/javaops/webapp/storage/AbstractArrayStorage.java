2f05c708c0a06fb91a9fde018c2d8cfe29d2d005
Mikhail Krasikov <mishkras@mail.ru>
2022-04-26 15:55:29 +0300

    HW3: finish

--- src/ru/javaops/webapp/storage/AbstractArrayStorage.java
+++ src/ru/javaops/webapp/storage/AbstractArrayStorage.java
@@ -72,6 +72,6 @@ public abstract class AbstractArrayStorage implements Storage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
+        return Arrays.copyOf(storage, size);
     }
 }
